ecb262cf4b0d870d2840abba0d4aa84ba1669c08
Isaque Silva <79009221+ZekaBoga@users.noreply.github.com>
2021-09-20 10:42:23 -0300

    aaaa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,56 @@
 import { Component, ɵisBoundToModule__POST_R3__ } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Todo } from 'src/models/todo.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  public todos: any[] = [];
+  public todos: Todo[] = [];
   public title: String = "Minhas Tarefas";
-  // public toDo: = String document.getElementById('todoID');
+  public form: FormGroup;
 
-  constructor() {
-    this.todos.push('passear com o cachorro');
-  }
-  alterarTexto() {
-    this.todos = []
-  }
-  removerTodo() {
-    this.todos.push("aaa");
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      title: ['', Validators.compose([
+        Validators.minLength(5),
+        Validators.maxLength(40),
+        Validators.required
+      ])]
+    });
+    this.todos.push(new Todo (1, "Passear com o cachorro" , false));
+    this.todos.push(new Todo (2, "Levar o lixo para fora" , false));
+    this.todos.push(new Todo (3, "Limpar o quarto"        , false));
   }
 
-  adicionarTodo() {
-    this.todos.push("aaa");
+  remove(todo: Todo) {
+    const index = this.todos.indexOf(todo);
+    if (index !== -1) {
+      this.todos.splice(index, 1);
+    }
   }
+
+  done(todo: Todo) {
+    todo.done = true
+  }
+
+  undone(todo: Todo) {
+    todo.done = false
+  }
+
+  add() {
+    const title = this.form.controls['title'].value;
+    const id = this.todos.length + 1;
+    this.todos.push(new Todo(id, title, false));
+    this.clear();
+  }
+
+  clear() {
+    this.form.reset();
+  }
+
 }
 
---
+++ src/models/todo.model.ts
@@ -0,0 +1,9 @@
+export class Todo {
+    constructor(
+
+        public id: Number,
+        public title: String,
+        public done: Boolean
+
+    ) { }
+}
